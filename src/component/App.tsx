021ea243c515f605f5f62c152648f363560e8b35
Ulrich Stark <ulrich.stark@gmx.de>
2021-03-23 10:17:44 +0100

    small changes

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -15,7 +15,6 @@ export const App = (props: IProps) => {
                     <li>React 17.0 + JSX</li>
                     <li>TypeScript 4.2</li>
                     <li>SCSS</li>
-                    <li>Yarn</li>
                 </ul>
             </section>
             <section>
