066e23813352469379c7bb332db36fcca4ded01d
Ulrich Stark <ulrich.stark@gmx.de>
2022-05-08 21:46:06 +0200

    fix tests and add @testing-library/user-event

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { App } from "./App";
 
 test("heading of App", () => {
@@ -8,13 +9,14 @@ test("heading of App", () => {
     expect(heading).toHaveTextContent("Hello World");
 });
 
-test("button of App", () => {
+test("button of App", async () => {
+    const user = userEvent.setup();
     const result = render(<App />);
     const button = result.getByRole("button");
 
     expect(button).toHaveTextContent("1");
 
-    fireEvent.click(button);
+    await user.click(button);
 
     expect(button).toHaveTextContent("2");
 });
