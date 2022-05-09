f2bef9270eef3ec23c7dafe92b653d3c991b11b2
Ulrich Stark <ulrich.stark@gmx.de>
2022-05-09 20:19:50 +0200

    fix type for vite config + improve sample test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,21 +2,23 @@ import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { App } from "./App";
 
-test("heading of App", () => {
-    const result = render(<App />);
-    const heading = result.getByRole("heading");
+describe("App component", () => {
+    test("contains heading", () => {
+        const result = render(<App />);
+        const heading = result.getByRole("heading");
 
-    expect(heading).toHaveTextContent("Hello World");
-});
+        expect(heading).toBeInTheDocument();
+    });
 
-test("button of App", async () => {
-    const user = userEvent.setup();
-    const result = render(<App />);
-    const button = result.getByRole("button");
+    test("button click increases counter", async () => {
+        const user = userEvent.setup();
+        const result = render(<App />);
+        const button = result.getByRole("button");
 
-    expect(button).toHaveTextContent("1");
+        expect(button).toHaveTextContent("1");
 
-    await user.click(button);
+        await user.click(button);
 
-    expect(button).toHaveTextContent("2");
+        expect(button).toHaveTextContent("2");
+    });
 });
